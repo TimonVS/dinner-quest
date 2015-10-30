de56414d61b721724da82f4e328e4677ada50c4f
Timon van Spronsen <timonvanspronsen@outlook.com>
2015-10-30 23:40:52 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  devtool: 'sourcemap',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+       { test: /\.js$/, exclude: [/app\/lib/, /node_modules/], loader: 'babel' },
+       { test: /\.html$/, loader: 'raw' },
+       { test: /\.styl$/, loader: 'style!css!stylus' },
+       { test: /\.scss$/, loader: 'style!css!sass' },
+       { test: /\.css$/, loader: 'style!css' }
+    ]
+  }
+};
