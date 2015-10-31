d02d782f266a429d32b769d44e2cb05ba44939d5
Madhusudhan Srinivasa <madhums8@gmail.com>
2015-10-31 11:32:03 +0100

    redirect user if he is already logged in

--- client/app/common/login/login.controller.js
+++ client/app/common/login/login.controller.js
@@ -4,6 +4,10 @@ class LoginController {
   constructor (User, $rootScope, $location) {
     this.name = 'Login';
 
+    if (User.getUser()) {
+      return $location.path('/');
+    }
+
     this.login = () => {
       User.Auth.login((data) => {
         $rootScope.user = data.facebook;
