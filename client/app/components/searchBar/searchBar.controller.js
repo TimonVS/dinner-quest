284eebd0269c6cc85e31939ac26a83cffce7263a
Timon van Spronsen <timonvanspronsen@outlook.com>
2015-11-01 00:21:59 +0100

    Add search bar directive

---
+++ client/app/components/searchBar/searchBar.controller.js
@@ -0,0 +1,11 @@
+class SearchBarController {
+  constructor() {
+    var vm = this
+
+    vm.activateSearch = function () {
+      vm.activated = true
+    }
+  }
+}
+
+export default SearchBarController;
