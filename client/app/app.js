276e8700bc3a5ca33a6be5b300f3e5fb23b5d764
Madhusudhan Srinivasa <madhums8@gmail.com>
2015-10-31 11:40:09 +0100

    bleh

--- client/app/app.js
+++ client/app/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import 'ionic-sdk/release/js/ionic.bundle.js'
+import 'ionic-sdk/release/js/ionic.bundle.js';
 import 'angularjs-slider';
 import uiRouter from 'angular-ui-router';
 import Common from './common/common';
@@ -8,7 +8,7 @@ import Components from './components/components';
 import AppComponent from './app.component';
 
 import 'normalize.css';
-import 'app.scss'
+import 'app.scss';
 
 angular.module('app', [
   uiRouter,
