14de03fd7ba05e230b46ee6d3b26781d3a707d2f
Timon van Spronsen <timonvanspronsen@outlook.com>
2015-10-31 01:05:44 +0100

    Try adding Ionic

--- client/app/app.js
+++ client/app/app.js
@@ -4,11 +4,15 @@ import Common from './common/common';
 import Components from './components/components';
 import AppComponent from './app.component';
 import 'normalize.css';
+import 'ionic-sdk/release/js/ionic.bundle.js'
+//import 'ionic-sdk/release/css/ionic.css'
+import 'app.scss'
 
 angular.module('app', [
   uiRouter,
   Common.name,
-  Components.name
+  Components.name,
+  'ionic'
 ])
 
 .directive('app', AppComponent);
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,38 @@
+import path from 'path'
+
 module.exports = {
   devtool: 'sourcemap',
   output: {
     filename: 'bundle.js'
+  },
+  resolve: {
+    root: [
+      path.join(__dirname, 'client', 'app'),
+      path.join(__dirname, 'bower_components'),
+      path.join(__dirname, 'node_modules'),
+    ],
+    moduleDirectories: [
+      'bower_components',
+      'node_modules',
+    ],
+    alias: {
+    }
   },
   module: {
     loaders: [
        { test: /\.js$/, exclude: [/app\/lib/, /node_modules/], loader: 'babel' },
        { test: /\.html$/, loader: 'raw' },
        { test: /\.styl$/, loader: 'style!css!stylus' },
-       { test: /\.scss$/, loader: 'style!css!sass' },
-       { test: /\.css$/, loader: 'style!css' }
+       { test: /\.scss$/, loader: 'style!css!sass?includePaths[]=' + (path.join(__dirname, 'node_modules'))},
+       { test: /\.css$/, loader: 'style!css' },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url?limit=100000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file'
+      }
     ]
   }
 };
