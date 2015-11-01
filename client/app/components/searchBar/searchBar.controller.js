672dcfe8f690f67d14a5310045f7a5a094e8d308
Timon van Spronsen <timonvanspronsen@outlook.com>
2015-11-01 01:05:49 +0100

    Improve searchbar

--- client/app/components/searchBar/searchBar.controller.js
+++ client/app/components/searchBar/searchBar.controller.js
@@ -1,6 +1,13 @@
 class SearchBarController {
-  constructor() {
-    var vm = this
+  constructor(Recepies) {
+    var vm = this;
+
+    vm.recipies = Recepies.query();
+
+    vm.chooseRecipy = function (recipy) {
+      vm.activated = false;
+      vm.search = recipy.recepttitel;
+    }
 
     vm.activateSearch = function () {
       vm.activated = true
